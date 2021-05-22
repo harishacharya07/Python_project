b603b7658154e85c195c48bd3325398151026fe4
harishacharya07 <harishacharyabkr@gmail.com>
2021-05-22 18:03:53 +0530

    py

--- hello word.py
+++ hello word.py
@@ -1,24 +1,19 @@
-def reverseCheck(number):
-    originalNum= number
-    reverseNum = 0
+def reverse(number):
+    original_number = number
+    reverse_number = 0
 
-
-    while(number > 0):
+    while number > 0:
         reminder = number % 10
-        reverseNum = (reverseNum * 10) + reminder
+        reverse_number = (reverse_number * 10) + reminder
 
-
-        if(originalNum == reverseNum):
+        if original_number == reverse:
             return True
         else:
             return False
 
 
 print("original and reverse number is")
-print(reverseCheck(121))
-
+print(reverse(121))
 print("hello word ")
-
-
 print("steel quantity estimation")
 print("hello word")
